e93682787969320e45b19308d4a0bb3641f506ed
Jurandir Santos <79405529+Thotscript@users.noreply.github.com>
2025-05-14 11:09:21 -0300

    Delete Wpptalk-Server-2.0 directory

--- Wpptalk-Server-2.0/middlewares/corsHeaders.js
+++
@@ -1,9 +0,0 @@
-// middlewares/corsHeaders.js
-export default function corsHeaders(req, res, next) {
-  res.header('Access-Control-Allow-Origin', 'https://thebroker.vip');
-  res.header('Access-Control-Allow-Headers', 'Content-Type');
-  res.header('Access-Control-Allow-Methods', 'POST, GET, OPTIONS');
-  res.header('Cross-Origin-Resource-Policy', 'cross-origin');
-  if (req.method === 'OPTIONS') return res.sendStatus(200);
-  next();
-}
--- Wpptalk-Server-2.0/routes/index.js
+++
@@ -1,5 +0,0 @@
-// routes/index.js
-import auth from './authRoutes.js';
-import filtros from './filtrosRoutes.js';
-
-export default { auth, filtros };
